d4decb7d413cc373186a14e0dec1b2ecc45e21a5
willrinehart <william.eric.rinehart@gmail.com>
2016-07-15 12:34:23 -0400

    update

--- election_modeling.R
+++ election_modeling.R
@@ -4,22 +4,17 @@ library(jsonlite)
 library(lubridate)
 
 ## API get | http://elections.huffingtonpost.com/pollster/api
-url <- paste0("http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1&page=", page)
-page <- 1
-huffpolls <- fromJSON(url)
-df <- as.data.frame(huffpolls)
-huffrows <- nrow(huffpolls) 
-
-if (huffrows <= 10) {
-  page = (page + 1)
-  print(page)
+baseurl <- "http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1"
+pages <- list()
+for(i in 0:30){
+  mydata <- fromJSON(paste0(baseurl, "&page=", i))
+  message("Retrieving page ", i)
+  pages[[i+1]] <- mydata
 }
 
-url <- paste0("http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1&page=", page)
-huffpolls <- fromJSON(url)
-df2 <- as.data.frame(huffpolls)
-df <- rbind(df, df2)
-huffrows <- nrow(huffpolls)  
+### combine all into one
+polls <- rbind.pages(pages)
+
 
 ### standard deviation calculation | http://faculty.vassar.edu/lowry/polls/poll4.html
 p <- .44
@@ -35,3 +30,18 @@ sim <- sum(sim >= 3 & sims <= 6)/runs
 
 
 
+#### NEW LOOP ####
+## API get | http://elections.huffingtonpost.com/pollster/api
+baseurl <- "http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1"
+pages <- list()
+for(i in i){
+  mydata <- fromJSON(paste0(baseurl, "&page=", i))
+  message("Retrieving page ", i)
+  pages[[i+1]] <- mydata
+}
+
+### combine all into one
+polls2 <- rbind.pages(pages)
+
+
+fromJSON(paste0(baseurl, "&page="
