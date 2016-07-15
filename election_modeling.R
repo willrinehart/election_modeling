18050d4f3e5f33689027b44a9d44b3261b03cb84
willrinehart <william.eric.rinehart@gmail.com>
2016-07-15 16:58:44 -0400

    update

--- election_modeling.R
+++ election_modeling.R
@@ -2,6 +2,34 @@
 ### load libraries
 library(jsonlite)
 library(lubridate)
+library(XML)
+library(pollstR)
+library(reshape)
+library(ggplot2)
+
+### API get with pollstR | https://cran.r-project.org/web/packages/pollstR/vignettes/introduction.html
+polldata <- pollstr_polls(max_pages = 10000, after = "2016-01-01")
+questions <- subset(polldata$questions, state=="US"
+                    & topic=="2016-president"  
+                    & subpopulation %in% c("Adults", "Likely Voters", "Registered Voters"))
+
+### merge data
+prespolldata <- merge(questions, polldata$polls, by = "id")
+
+### standard deviation calculation
+prespolldata$sd <- 100 * (sqrt((prespolldata$value/100)*(1-(prespolldata$value/100))/prespolldata$observations))
+
+### subset data for each candidate
+clintonpres <- subset(prespolldata, choice=="Clinton")
+trumppres <- subset(prespolldata, choice=="Trump")
+other <- subset(prespolldata, choice=="Trump")
+
+
+### load libraries
+library(jsonlite)
+library(lubridate)
+library(XML)
+library(pollstR)
 
 ## API get | http://elections.huffingtonpost.com/pollster/api
 baseurl <- "http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1"
@@ -14,6 +42,8 @@ for(i in 0:30){
 
 ### combine all into one
 polls <- rbind.pages(pages)
+
+questions <- xmlToList(polls$questions)
 
 
 ### standard deviation calculation | http://faculty.vassar.edu/lowry/polls/poll4.html
@@ -30,18 +60,23 @@ sim <- sum(sim >= 3 & sims <= 6)/runs
 
 
 
-#### NEW LOOP ####
-## API get | http://elections.huffingtonpost.com/pollster/api
-baseurl <- "http://elections.huffingtonpost.com/pollster/api/polls.json?topic=2016-president&after=2015-6-1"
-pages <- list()
-for(i in i){
-  mydata <- fromJSON(paste0(baseurl, "&page=", i))
-  message("Retrieving page ", i)
-  pages[[i+1]] <- mydata
-}
+#### NEW CODE ####
+library(XML)
+library(reshape)
+library(ggplot2)
 
-### combine all into one
-polls2 <- rbind.pages(pages)
+### API get with pollstR | https://cran.r-project.org/web/packages/pollstR/vignettes/introduction.html
+polldata <- pollstr_polls(max_pages = 10000, after = "2016-1-1")
+questions <- subset(polldata$questions, state=="US" 
+                    & topic=="2016-president" 
+                    & subpopulation %in% c("Adults", "Likely Voters", "Registered Voters"))
+
+prespolldata <- merge(polldata$polls, questions, by = "id")
+
+### standard deviation calculation
+questions$sd <- 100 * (sqrt((questions$value/100)*(1-(questions$value/100))/questions$observations))
 
 
-fromJSON(paste0(baseurl, "&page="
+dat <- pollstR(pages=20)
+ggplot(dat,aes(end.date,Obama/(Obama+Romney)))+geom_point(alpha=.5)+geom_smooth(aes(weight=sqrt(N)))+geom_hline(aes(yintercept=0.5),lty=2,size=1)+
+  labs(title="Proportion of Vote for Obama",x="Last Date of Poll",y=NULL)
